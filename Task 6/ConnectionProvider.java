6d8a59f8608e70361de70c0a75f22fd1c9615e56
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-04-27 22:52:05 +0530

    Update ConnectionProvider.java

--- Task 6/ConnectionProvider.java
+++ Task 6/ConnectionProvider.java
@@ -14,7 +14,7 @@ public class ConnectionProvider {
         try
         {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/bms","root","admin321");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/bms","root","admin123");
             return con;
         }
         catch(Exception e)
